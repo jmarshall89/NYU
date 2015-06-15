441985f88e8f1edfeaaea24e07ed4bd69fbe763c
jmarshall89 <jmarshall@learnvest.com>
2015-06-15 13:36:46 -0400

    Changed courses logic to create a unique course object for each student. This allows us to have unique grades for classes.

--- NYUJavaClass/src/StudentTest.java
+++ NYUJavaClass/src/StudentTest.java
@@ -12,13 +12,13 @@ public class StudentTest {
         students[4] = new CollegeStudent("Chris", "Kempel");
         students[5] = new CollegeStudent("David", "Clarke");
 
-//        Create some courses
-        Course algebra = new Course(1234, "Basic number game", "Hall");
-        Course english = new Course(1235, "Spelling and grammar", "Fignerski");
-        Course art = new Course(1236, "Learning about art", "Scott");
-        Course calculus = new Course(101, "Don't drink and derieve", "Bayne", 50);
-        Course business = new Course(103, "Learn about business", "Whaley", 50);
-        Course philosphy = new Course(203, "Get an easy A", "Toto", 50);
+////        Create some courses, and a new one for each student
+//        Course algebra = new Course(1234, "Basic number game", "Hall");
+//        Course english = new Course(1235, "Spelling and grammar", "Fignerski");
+//        Course art = new Course(1236, "Learning about art", "Scott");
+//        Course calculus = new Course(101, "Don't drink and derieve", "Bayne", 50);
+//        Course business = new Course(103, "Learn about business", "Whaley", 50);
+//        Course philosphy = new Course(203, "Get an easy A", "Toto", 50);
 
         /*  All high school studetns will take the high school classes, and all the college students
             will take the college classes
@@ -31,16 +31,27 @@ public class StudentTest {
         double stdev = 5.0f;
         RandomGrade grade = new RandomGrade(mean, stdev);
 
+        /* This is not a solid way to actually add courses. A more complicated yet solid way
+        would be to have a private static final value in each student subclass which tells the
+        type of student, then do a switch-case to add the right classes.
+         */
         for (int i = 0; i < 3; i++){
-            students[i].addCourse(algebra);
-            students[i].addCourse(english);
-            students[i].addCourse(art);
+//        Create some courses, and a new one for each student
+            Course tempAlgebra = new Course(1234, "Basic number game", "Hall");
+            Course tempEnglish = new Course(1235, "Spelling and grammar", "Fignerski");
+            Course tempArt = new Course(1236, "Learning about art", "Scott");
+            students[i].addCourse(tempAlgebra);
+            students[i].addCourse(tempEnglish);
+            students[i].addCourse(tempArt);
         }
 
         for (int i = 3; i <= 5; i++) {
-            students[i].addCourse(calculus);
-            students[i].addCourse(business);
-            students[i].addCourse(philosphy);
+            Course tempCalculus = new Course(101, "Don't drink and derieve", "Bayne", 50);
+            Course tempBusiness = new Course(103, "Learn about business", "Whaley", 50);
+            Course tempPhilosphy = new Course(203, "Get an easy A", "Toto", 50);
+            students[i].addCourse(tempCalculus);
+            students[i].addCourse(tempBusiness);
+            students[i].addCourse(tempPhilosphy);
         }
 
 //        now set the grade for the courses
