1daa79dccb0826b6269a45117150901ffb80448a
jmarshall89 <jmarshall@learnvest.com>
2015-06-30 16:57:50 -0400

    Put in debugging values so the code can be ran much quicker

--- Blackjack/src/Game.java
+++ Blackjack/src/Game.java
@@ -21,23 +21,27 @@ public class Game {
 //        Game on!
         gameOn = true;
         game.getIntro();
-        decks = gameI.setDecks();
-        numPlayers = gameI.setPlayerNumber();
-
+//        decks = gameI.setDecks();
+        decks = 2; // todo delete for debugging
+//        numPlayers = gameI.setPlayerNumber();
+        numPlayers = 2; // todo delete for debugging
 //        Now create the players and deck
         dd = new DealerDeck(decks);
-        String name = gameI.setName();
+//        String name = gameI.setName();
+        String name = "Josh"; // todo delete for debugging
         AIPlayers ai = new AIPlayers(numPlayers, name);
         players = ai.getPlayers();
 
 //        Finally the player can set their bankroll
-        players.get(0).setBank(gameI.setBank());
+//        players.get(0).setBank(gameI.setBank());
+        players.get(0).setBank(100); // todo delete for debugging
 
 //        Now the game logic
         while (gameOn) {
 
 //            Set player bet
-            players.get(0).setBet(gameI.setBet(players.get(0).getName()));
+//            players.get(0).setBet(gameI.setBet(players.get(0).getName()));
+            players.get(0).setBet(10); // todo delete for debugging
             ai.initialDeal(dd);
             game.playerPlay(players.get(0),gameI, dd);
             ai.aiPlay(dd);
