cd1f2f6b96942c9d0a4cae00924abc8a792a9b1c
jmarshall89 <jmarshall@learnvest.com>
2015-08-04 17:42:18 -0400

    New class plus start of GUI

---
+++ NYUJavaClass/IO with Database/src/MainDBParser.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jmarshall on 8/4/15.
+ */
+public class MainDBParser {
+
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller();
+
+        
+    }
+
+}
--- NYUJavaClass/IO with Database/src/View.java
+++ NYUJavaClass/IO with Database/src/View.java
@@ -1,7 +1,41 @@
+import javax.swing.*;
+
 /**
  * Created by jmarshall on 7/27/15.
  */
-public class View {
+public class View extends JFrame {
+
+    JLabel nameLabel = new JLabel("Search Last Name:");
+    JTextField name = new JTextField(17);
+    JLabel output = new JLabel("");
+    JButton enter = new JButton("ENTER");
+    JButton cancel = new JButton("CANCEL");
+
+    public View() {
+
+        setTitle("Enter a last name to search.");
+        setBounds(150, 50, 280, 350);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel pane = new JPanel();
+        setContentPane(pane);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     public String getUserInput() {
         String name;
